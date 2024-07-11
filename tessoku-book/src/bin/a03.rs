cda6a626748894898cdee29bb9598bccaca1fc00
ugr0 <yujiro.takahashi0912@gmail.com>
2024-07-11 22:39:19 +0900

    submit

--- tessoku-book/src/bin/a03.rs
+++ tessoku-book/src/bin/a03.rs
@@ -1,3 +1,24 @@
+use proconio::input;
+
 fn main() {
-    todo!();
+    input! {
+        n: usize,
+        k: i32,
+        p: [i32; n],
+        q: [i32; n]
+    }
+
+    let mut exist = false;
+    for i in 0..n {
+        for j in 0..n {
+            if p[i] + q[j] == k {
+                exist = true;
+            }
+        }
+    }
+    if exist {
+        println!("Yes");
+    } else {
+        println!("No");
+    }
 }
