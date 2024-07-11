efa7610dd99ffea7ea1cd156ee1e260d87b6c89c
ugr0 <yujiro.takahashi0912@gmail.com>
2024-07-11 23:43:04 +0900

    submit

--- tessoku-book/src/bin/a06.rs
+++ tessoku-book/src/bin/a06.rs
@@ -1,3 +1,28 @@
+use proconio::input;
+
 fn main() {
-    todo!();
+    input! {
+        n: usize,
+        q: usize,
+        a: [i32; n],
+        range: [(usize,usize); q]
+    }
+
+    let range = range
+        .iter()
+        .map(|(l, r)| (l - 1, r - 1))
+        .collect::<Vec<(usize, usize)>>();
+
+    let mut sum = vec![0; n];
+    for i in 0..n {
+        sum[i] = a[i];
+        if i > 0 {
+            sum[i] += sum[i - 1];
+        }
+    }
+
+    for (l, r) in range {
+        let ans = if l == 0 { sum[r] } else { sum[r] - sum[l - 1] };
+        println!("{}", ans);
+    }
 }
