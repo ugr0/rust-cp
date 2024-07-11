982474652993781708baf4c9d8617e43a5368f97
ugr0 <yujiro.takahashi0912@gmail.com>
2024-07-11 22:26:13 +0900

    submit

--- tessoku-book/src/bin/a02.rs
+++ tessoku-book/src/bin/a02.rs
@@ -1,3 +1,21 @@
+use proconio::input;
 fn main() {
-    todo!();
+    input! {
+        n: usize,
+        x: i32,
+        a: [i32; n]
+    }
+    
+    let mut ans = false;
+    for i in 0..n {
+        if a[i] == x {
+            ans = true;
+        }
+    }
+    
+    if ans {
+        println!("Yes")
+    } else {
+        println!("No")
+    }
 }
