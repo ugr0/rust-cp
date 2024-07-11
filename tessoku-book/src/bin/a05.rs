cbd13c0b34fd4211a22829b25ab42cfc5dbf3ed5
ugr0 <yujiro.takahashi0912@gmail.com>
2024-07-11 23:09:20 +0900

    submit

--- tessoku-book/src/bin/a05.rs
+++ tessoku-book/src/bin/a05.rs
@@ -1,3 +1,19 @@
+use proconio::input;
 fn main() {
-    todo!();
+    input! {
+        n: i32,
+        k: i32
+    }
+
+    let mut ans = 0;
+    for i in 1..(n + 1) {
+        for j in 1..(n + 1) {
+            let tmp: i32 = k - i - j;
+            if tmp > 0 && tmp <= n {
+                ans += 1;
+            }
+        }
+    }
+
+    println!("{}", ans);
 }
